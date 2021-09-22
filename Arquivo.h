979ee7831e53ce8a743a31931b19fed02c247b88
LuckyLucario <danielkttateishi@gmail.com>
2021-09-22 18:34:51 -0300

    Adicionando comentarios gerais

--- Arquivo.h
+++ Arquivo.h
@@ -31,8 +31,19 @@ public:
 class ArquivoFIX: public Arquivo
 {
 private:
-    int offset_reg;
+    int offset_reg; 
     int offset_cam[];
+    /*
+        Total(100 bytes offset)
+        KEY (i.e., número que identifica a pessoa) (4 bytes)
+        LASTNAME (i.e., sobrenome da pessoa) (16 bytes)
+        FIRSTNAME (i.e., primeiro nome da pessoa) (16 bytes)
+        ADDRESS (i.e., endereço com logradouro, número e complemento) (32 bytes)
+        CITY (i.e., cidade) (20 bytes)
+        STATE (i.e., sigla do estado com 2 caracteres, tal como SP) (2 bytes)
+        ZIP (i.e., CEP, tal como 222222-222) (4 bytes)
+        PHONE (i.e., número do telefone com DDD, tal como (022)2222-2222) (6 bytes)
+    */
 
 public:
     ArquivoFIX(string p, char t);
--- Registro.h
+++ Registro.h
@@ -31,14 +31,15 @@ private:
     } phone;
     
 /*
-KEY (i.e., número que identifica a pessoa)
-LASTNAME (i.e., sobrenome da pessoa)
-FIRSTNAME (i.e., primeiro nome da pessoa)
-ADDRESS (i.e., endereço com logradouro, número e complemento)
-CITY (i.e., cidade)
-STATE (i.e., sigla do estado com 2 caracteres, tal como SP)
-ZIP (i.e., CEP, tal como 222222-222)
-PHONE (i.e., número do telefone com DDD, tal como (022)2222-2222)
+Total(100 bytes offset)
+KEY (i.e., número que identifica a pessoa) (4 bytes)
+LASTNAME (i.e., sobrenome da pessoa) (16 bytes)
+FIRSTNAME (i.e., primeiro nome da pessoa) (16 bytes)
+ADDRESS (i.e., endereço com logradouro, número e complemento) (32 bytes)
+CITY (i.e., cidade) (20 bytes)
+STATE (i.e., sigla do estado com 2 caracteres, tal como SP) (2 bytes)
+ZIP (i.e., CEP, tal como 222222-222) (4 bytes)
+PHONE (i.e., número do telefone com DDD, tal como (022)2222-2222) (6 bytes)
 */
 
 
